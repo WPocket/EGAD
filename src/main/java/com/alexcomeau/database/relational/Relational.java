30b9ecd7de1a64f870d363f1ad528b298bd587c9
Alex Comeau <17982529+alexm622@users.noreply.github.com>
2021-11-07 15:07:54 -0500

    started mysql

--- src/main/java/com/alexcomeau/database/relational/Relational.java
+++ src/main/java/com/alexcomeau/database/relational/Relational.java
@@ -1,5 +1,22 @@
 package com.alexcomeau.database.relational;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.apache.commons.lang3.tuple.Pair;
+
 public interface Relational {
+    public void connect();
+    public void disconnect();
+
+    public void insert(String table, HashMap<String, Pair<String, String>> values);
     
+    public ArrayList<String> select(String value);
+
+    public ArrayList<HashMap<String, String>> selectMultiple(ArrayList<String> multiple);
+
+    public ArrayList<String> selectWhere(String single, HashMap<String, String> where);
+    public ArrayList<HashMap<String, String>> selectMultipleWhere(ArrayList<String> multiple, HashMap<String, String> where);
+
+    public boolean newTable(String name, ArrayList<String> types, HashMap<String, String> options);
 }
--- src/main/java/com/alexcomeau/database/relational/mysql/MySql.java
+++ src/main/java/com/alexcomeau/database/relational/mysql/MySql.java
@@ -1,7 +1,18 @@
 package com.alexcomeau.database.relational.mysql;
 
+import com.alexcomeau.database.DatabaseExecption;
 import com.alexcomeau.database.relational.Relational;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class MySql implements Relational{
-    
+    private Connection con;
+    public MySql() throws DatabaseExecption{
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+        }catch(Exception e){
+            throw new DatabaseExecption("01", e.getMessage(), e);
+        }
+    }
 }
