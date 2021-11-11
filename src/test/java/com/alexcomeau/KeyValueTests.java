7f94a787fa5d843acb1dcb795b91d53e22792b97
Alex Comeau <17982529+alexm622@users.noreply.github.com>
2021-11-11 18:51:23 -0500

    updated build name and removed a few errors

--- src/test/java/com/alexcomeau/KeyValueTests.java
+++ src/test/java/com/alexcomeau/KeyValueTests.java
@@ -6,13 +6,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import java.util.ArrayList;
 
-
-import com.alexcomeau.config.Database;
-import com.alexcomeau.database.keyvalue.KVFactory;
 import com.alexcomeau.database.keyvalue.KeyValue;
 import com.alexcomeau.database.keyvalue.redis.Redis;
-import com.alexcomeau.database.relational.Relational;
-import com.alexcomeau.database.relational.RelationalFactory;
 
 import org.junit.jupiter.api.Test;
 
@@ -21,7 +16,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-import redis.clients.jedis.exceptions.JedisConnectionException;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -42,7 +36,7 @@ public class KeyValueTests {
 	public void KeyValueSelectIndex() throws Exception {
         Main.kv = new ArrayList<KeyValue>();
         Main.kv.add(new Redis(null));
-        
+
 		this.mockMvc.perform(get("/kv/0/get/key=test")).andDo(print()).andExpect(status().isOk());
 	}
     
